9b34161d6525864fb4eed8b091bc1e1ed2cc07a8
fillytewe1 <135631067+fillytewe1@users.noreply.github.com>
2023-06-15 18:23:40 +0200

    Add files via upload

---
+++ ibanchecker/ibancheckerTest/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ibancheckerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtIBAN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIBAN = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIBAN
+            // 
+            this.txtIBAN.Location = new System.Drawing.Point(95, 83);
+            this.txtIBAN.Name = "txtIBAN";
+            this.txtIBAN.Size = new System.Drawing.Size(233, 22);
+            this.txtIBAN.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IBAN : ";
+            // 
+            // btnIBAN
+            // 
+            this.btnIBAN.Location = new System.Drawing.Point(361, 77);
+            this.btnIBAN.Name = "btnIBAN";
+            this.btnIBAN.Size = new System.Drawing.Size(144, 35);
+            this.btnIBAN.TabIndex = 2;
+            this.btnIBAN.Text = "IBAN Überprüfen";
+            this.btnIBAN.UseVisualStyleBackColor = true;
+            this.btnIBAN.Click += new System.EventHandler(this.btnIBAN_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 164);
+            this.Controls.Add(this.btnIBAN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIBAN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIBAN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIBAN;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ibanchecker/ibancheckerTest/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace ibancheckerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public BigInteger Modulo ( BigInteger nummerein, BigInteger nummerzwei) 
+        {
+            return nummerein % nummerzwei;
+        }
+
+        private void btnIBAN_Click(object sender, EventArgs e)
+        {
+
+            string pattern = @"DE[0-9]{11,}";// Create a Regex  // picture box // textbox control das eigene eingaben key down event ! regex üverprüfen 
+            string ibanEingabe = Regex.Replace(txtIBAN.Text.ToString(), "\\s+", ""); // falls es space enthält werden sie hier entfernt 
+            bool ibanSizeBool = ibanEingabe.Length <= 22;
+            int ibanLaengeAlsInt = ibanEingabe.Length; // Sollte 22 sein
+            string pruefziffer = ibanEingabe.Substring(2, 2); //DE(45)
+            bool boolpruefziffer = Convert.ToInt32(pruefziffer) > 1 && Convert.ToInt32(pruefziffer) < 99; // Ist nummer zwischen 2 und 98?
+            if ((Regex.IsMatch(ibanEingabe,pattern)) && (ibanSizeBool) && (boolpruefziffer)) // Falls Iban >= 13 aber kleiner < 22  ist werden nullen hinzugefügt bis es 22 Ziffern lang ist
+            {
+                int aktuelleIbanLaengeUngleichNull = 22 - ibanLaengeAlsInt; // 0 heißt es sind 22 != 0 heißt es fehlen diese anzahl von Kontonummer.3 == die zahl die hinzugefügt werden muss 
+                int aktuelleKontonummerLaengeUNgleichNull = 10 - aktuelleIbanLaengeUngleichNull; // KN sollte 10 sein deshsalb
+                string startUndEndeVonkleineKontonummer = ibanEingabe.Substring(12, aktuelleKontonummerLaengeUNgleichNull); // KontonummerStart 12 bis (+) z.b 3 
+                int kleineKontonummerLaengeInt = startUndEndeVonkleineKontonummer.Length;
+                string kleineIban = ibanEingabe.Substring(0, ibanLaengeAlsInt); // kontonummer
+                if (kleineKontonummerLaengeInt < 10)
+                {
+                    for (int g = ibanLaengeAlsInt; g < 22; g++)
+                    {
+                        string testnull = "0";
+                        kleineIban = kleineIban + testnull;
+                    }
+                } //Iban wird zusammengesetz (DE00 Rückt nach vorne etc) .Bereitstellung für Rechnung
+                string neueIban = kleineIban;
+                string restDerIbanNachPruefziffer = neueIban.Substring(4, 18);
+                string deKonvertiertUndNullen = "131400"; //DE50 1000 0000 1234 5678 89//DE50100000001234567889                     
+                string neueIbanDurchInsertAmEndeDerKontonummer = restDerIbanNachPruefziffer.Insert(18, deKonvertiertUndNullen); // Komplett neue Iban Mit 131400 am Ende der Kontonummer
+                // Rechnung und Kovertierung 
+                int prueffzifferToInt = Convert.ToInt32(pruefziffer);
+                BigInteger neuBigIntIban = BigInteger.Parse(neueIbanDurchInsertAmEndeDerKontonummer);
+                BigInteger moduloZahl = new BigInteger(97);
+                BigInteger bigIntRechnungIbanUndModuloZahl = Modulo(neuBigIntIban, moduloZahl);
+                string insertFuehrendeNullErgebnis = "";
+                BigInteger subtraktionVonModuloErgebnis = 98 - bigIntRechnungIbanUndModuloZahl;
+                string convertSubtraktionsErgebnistoString = subtraktionVonModuloErgebnis.ToString(); // Convert Big INt To String
+                insertFuehrendeNullErgebnis = convertSubtraktionsErgebnistoString;
+                int convertingBackToInt = Convert.ToInt32(insertFuehrendeNullErgebnis);
+                if (convertingBackToInt != prueffzifferToInt)
+                {
+                    MessageBox.Show(" NO!! Prüfziffer ist falsch. IBAN ist nicht valide");
+                }
+                else
+                {
+                    MessageBox.Show("YES !! Prüfziffer ist valide");
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(this.txtIBAN,$"Iban muss min. 13 bis max. 22 Ziffern sein.Ihre ist aktuell {ibanLaengeAlsInt}  Ziffern lang.DE Großschreiben + nach DE nur noch Zahlen bitte");
+            }
+            
+            
+        }
+    }
+}
